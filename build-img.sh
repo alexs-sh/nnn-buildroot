8d2e219eb912412a2409f1c62ad97cfb668b5623
Alexander Shirokov <shirokovalexs@gmail.com>
2025-08-20 09:15:00 +0200

    build:allow optional readline library

--- build-img.sh
+++ build-img.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 DEFCONFIG=qemu_aarch64_virt_defconfig
+MODE=defaut
 
 if [ "$#" -ge 1 ]; then
    DEFCONFIG=$1
 fi
+
+if [ "$#" -ge 2 ]; then
+   MODE=$2
+fi
+
 
 readonly BR_CONFIG=buildroot/configs/"$DEFCONFIG"
 
@@ -13,6 +19,11 @@ set -e
 
 echo "Add config $DEFCONFIG"
 cp configs/"$DEFCONFIG" "$BR_CONFIG"
+
+if [ "$MODE" == "with-readline" ]; then
+   echo "Add readline library"
+   echo "BR2_PACKAGE_READLINE=y" >> "$BR_CONFIG"
+fi
 
 echo "Apply config $DEFCONFIG"
 cd buildroot
